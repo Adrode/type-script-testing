f200348cfd82a7bfe76ce26f854887d1b18e4c91
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-02-03 21:53:13 +0100

    readonly object property

--- script.js
+++ script.js
@@ -1,132 +1,170 @@
 "use strict";
-const getArea = (shape) => {
-    if (shape.kind === "square") {
-        return Math.pow(shape.sideLength, 2);
-    }
-    if (shape.kind === "circle") {
-        return Math.PI * (Math.pow(shape.radius, 2));
-    }
-};
+//interfaces and types
+{
+    const getArea = (shape) => {
+        if (shape.kind === "square") {
+            return Math.pow(shape.sideLength, 2);
+        }
+        if (shape.kind === "circle") {
+            return Math.PI * (Math.pow(shape.radius, 2));
+        }
+    };
+}
 //generic functions
-const firstElement = (array) => array[0];
-//here function knows that firstNumber is a number or undefined type
-const numbers = [4, 9];
-const firstNumber = firstElement(numbers);
-console.log(`First number: ${firstNumber}`);
-//here function knows that firstAnimal is a string or undefined type
-const animals = ["Dog", "Parrot", "Monkey"];
-const firstAnimal = firstElement(animals);
-const move = (animal) => {
-    var _a, _b;
-    if ("fly" in animal) {
-        (_a = animal.fly) === null || _a === void 0 ? void 0 : _a.call(animal);
-    }
-    else {
-        (_b = animal.swim) === null || _b === void 0 ? void 0 : _b.call(animal);
-    }
-};
+{
+    const firstElement = (array) => array[0];
+    //here function knows that firstNumber is a number or undefined type
+    const numbers = [4, 9];
+    const firstNumber = firstElement(numbers);
+    console.log(`First number: ${firstNumber}`);
+    //here function knows that firstAnimal is a string or undefined type
+    const animals = ["Dog", "Parrot", "Monkey"];
+    const firstAnimal = firstElement(animals);
+}
+//"in" operator
+{
+    const move = (animal) => {
+        var _a, _b;
+        if ("fly" in animal) {
+            (_a = animal.fly) === null || _a === void 0 ? void 0 : _a.call(animal);
+        }
+        else {
+            (_b = animal.swim) === null || _b === void 0 ? void 0 : _b.call(animal);
+        }
+    };
+}
 //"is" operator
-/* type Dog = { run: () => void; };
-type Shark = { swim: () => void; };
+{
+    /* type Dog = { run: () => void; };
+    type Shark = { swim: () => void; };
 
-let animal: Dog | Shark = getPet();
+    let animal: Dog | Shark = getPet();
 
-const isDog = (pet: Dog | Shark): pet is Dog => "run" in pet;
+    const isDog = (pet: Dog | Shark): pet is Dog => "run" in pet;
 
-if (isDog(animal)) {
-    animal.run();
-} else {
-    animal.swim();
-} */
+    if (isDog(animal)) {
+        animal.run();
+    } else {
+        animal.swim();
+    } */
+}
 //basic functions
-const greeter = (greetFunction) => {
-    console.log(greetFunction("Hej!"));
-};
-const printToConsole = (text) => text;
-greeter(printToConsole);
+{
+    const greeter = (greetFunction) => {
+        console.log(greetFunction("Hej!"));
+    };
+    const printToConsole = (text) => text;
+    greeter(printToConsole);
+}
 //function with 2 types of arguments
-const map = (array, transformFunction) => array.map(transformFunction);
-const floatNumbers = [2.15, 4.76];
-const transformedNumbers = map(floatNumbers, (number) => number.toFixed(1));
-console.log(transformedNumbers);
+{
+    const map = (array, transformFunction) => array.map(transformFunction);
+    const floatNumbers = [2.15, 4.76];
+    const transformedNumbers = map(floatNumbers, (number) => number.toFixed(1));
+    console.log(transformedNumbers);
+}
 //function that accepts specific types of arguments
 //this function takes 2 values and return one, which is longer
-const longer = (value1, value2) => {
-    if (value1 > value2) {
-        return value1;
-    }
-    else {
-        return value2;
-    }
-};
-console.log(longer("xzxz", "xxx"));
-console.log(longer([], [1]));
+{
+    const longer = (value1, value2) => {
+        if (value1 > value2) {
+            return value1;
+        }
+        else {
+            return value2;
+        }
+    };
+    console.log(longer("xzxz", "xxx"));
+    console.log(longer([], [1]));
+}
 //function that merges 2 tables
-const mergeArrays = (array1, array2) => [
-    ...array1,
-    ...array2
-];
-console.log(mergeArrays([1, 2], ["opaska", "małpa"]));
+{
+    const mergeArrays = (array1, array2) => [
+        ...array1,
+        ...array2
+    ];
+    console.log(mergeArrays([1, 2], ["opaska", "małpa"]));
+}
 //functions that filters passed table
 //2 types of functions for example
-const filterArray1 = (array, filteringFunction) => array.filter(filteringFunction);
-const filterArray2 = (array, filterFunction) => array.filter(filterFunction);
-const numbers2 = [1, 2, 3, 4];
-const filteredNumbers1 = filterArray1(numbers2, (number) => number % 2 === 0);
-const filteredNumbers2 = filterArray2(numbers2, (number) => number % 2 === 0);
-console.log(filteredNumbers1);
-console.log(filteredNumbers2);
+{
+    const filterArray1 = (array, filteringFunction) => array.filter(filteringFunction);
+    const filterArray2 = (array, filterFunction) => array.filter(filterFunction);
+    const numbers2 = [1, 2, 3, 4];
+    const filteredNumbers1 = filterArray1(numbers2, (number) => number % 2 === 0);
+    const filteredNumbers2 = filterArray2(numbers2, (number) => number % 2 === 0);
+    console.log(filteredNumbers1);
+    console.log(filteredNumbers2);
+}
 //optional arguments
 //argument name can be a string or an undefined, that's why we need a ternary
-const greet = (name) => {
-    console.log(`Hi${name !== undefined ? `, ${name}` : ""}`);
-};
-greet("Adrian");
-greet();
+{
+    const greet = (name) => {
+        console.log(`Hi${name !== undefined ? `, ${name}` : ""}`);
+    };
+    greet("Adrian");
+    greet();
+}
 //function like map without using .map()
-const customMap = (array, mapFunction) => {
-    const newArray = [];
-    array.forEach((item, index) => {
-        newArray.push(mapFunction(item, index));
-    });
-    return newArray;
-};
-const animals2 = ["monkey", "donkey"];
-console.log(animals2.map((animal) => `${animal}`));
-console.log(customMap([1, 2, 3, 4, 5, 6], item => item + 1));
+{
+    const customMap = (array, mapFunction) => {
+        const newArray = [];
+        array.forEach((item, index) => {
+            newArray.push(mapFunction(item, index));
+        });
+        return newArray;
+    };
+    const animals2 = ["monkey", "donkey"];
+    console.log(animals2.map((animal) => `${animal}`));
+    console.log(customMap([1, 2, 3, 4, 5, 6], item => item + 1));
+}
 //type object
 //type object is everything that IS NOT a primitive type
-const value = [];
+{
+    const value = [];
+}
 //type unknown
-/* const function1 = (value: any) => {
-    value.doSomething();
+{
+    const function1 = (value) => {
+        value.doSomething();
+    };
+    const function2 = (value) => {
+        //value.doSomething();
+    };
+    const parseJSON = (jsonString) => JSON.parse(jsonString);
+    const array = parseJSON("blahBlah");
+    //array.push();
 }
-
-const function2 = (value: unknown) => {
-    //value.doSomething();
-}
-
-const parseJSON = (jsonString: string): unknown => JSON.parse(jsonString);
-const array = parseJSON("blahBlah");
-array.push(); */
 //if we have any, we can do whatever we want to a value
 //if we have unknown, we can't do anything to a value
 //type never
 //if a function return nothing, then TS acknowledges it as type never
-const error = () => {
-    throw new Error("Something went wrong!");
-};
+{
+    const error = () => {
+        throw new Error("Something went wrong!");
+    };
+}
 //type function
 //this type defines any function, that gets any arguments and returns any value
 //IT IS NOT RECOMENDED TO USE IT!
-const doSomething2 = (someFunction) => someFunction("text");
-const doSomething3 = (someFunction) => someFunction("text");
-//rest parameters
-const multiply = (factor, ...numbers) => numbers.map((number) => factor * number);
-const numbers3 = [1, 2, 3];
-console.log(multiply(5, ...numbers3));
-const numbersToPow = [2, 5];
-console.log(Math.pow(...numbersToPow));
+{
+    const doSomething2 = (someFunction) => someFunction("text");
+    const doSomething3 = (someFunction) => someFunction("text");
+    //rest parameters
+    const multiply = (factor, ...numbers) => numbers.map((number) => factor * number);
+    const numbers3 = [1, 2, 3];
+    console.log(multiply(5, ...numbers3));
+    const numbersToPow = [2, 5];
+    console.log(Math.pow(...numbersToPow));
+}
 //destructuring of parameters
-const rectangleArea = ({ sideA, sideB }) => sideA * sideB;
-console.log(rectangleArea({ sideA: 5, sideB: 6 }));
+{
+    const rectangleArea = ({ sideA, sideB }) => sideA * sideB;
+    console.log(rectangleArea({ sideA: 5, sideB: 6 }));
+}
+//assigning functions with void
+{
+    const doNothing = () => { };
+    const returnTrue = () => true;
+    const valueTrue = returnTrue();
+}
--- script.ts
+++ script.ts
@@ -1,226 +1,268 @@
 //interfaces and types
-interface Circle {
-    kind: "circle";
-    radius: number;
-}
-
-interface Square {
-    kind: "square";
-    sideLength: number;
-}
-
-type Shape = Circle | Square;
-
-const getArea = (shape: Shape) => {
-    if (shape.kind === "square") {
-        return shape.sideLength ** 2;
+{
+    interface Circle {
+        kind: "circle";
+        radius: number;
     }
 
-    if (shape.kind === "circle") {
-        return Math.PI * (shape.radius ** 2);
+    interface Square {
+        kind: "square";
+        sideLength: number;
+    }
+
+    type Shape = Circle | Square;
+
+    const getArea = (shape: Shape) => {
+        if (shape.kind === "square") {
+            return shape.sideLength ** 2;
+        }
+
+        if (shape.kind === "circle") {
+            return Math.PI * (shape.radius ** 2);
+        }
     }
 }
 
 //generic functions
-const firstElement = <ItemType>(array: ItemType[]): ItemType | undefined =>
-    array[0];
+{
+    const firstElement = <ItemType>(array: ItemType[]): ItemType | undefined =>
+        array[0];
 
-//here function knows that firstNumber is a number or undefined type
-const numbers = [4, 9];
-const firstNumber = firstElement(numbers);
-console.log(`First number: ${firstNumber}`);
+    //here function knows that firstNumber is a number or undefined type
+    const numbers = [4, 9];
+    const firstNumber = firstElement(numbers);
+    console.log(`First number: ${firstNumber}`);
 
-//here function knows that firstAnimal is a string or undefined type
-const animals = ["Dog", "Parrot", "Monkey"];
-const firstAnimal = firstElement(animals);
+    //here function knows that firstAnimal is a string or undefined type
+    const animals = ["Dog", "Parrot", "Monkey"];
+    const firstAnimal = firstElement(animals);
+}
 
 //"in" operator
-type Fish = { swim: () => void; };
-type Bird = { fly: () => void; };
-type Human = { fly?: () => void; swim?: () => void; };
+{
+    type Fish = { swim: () => void; };
+    type Bird = { fly: () => void; };
+    type Human = { fly?: () => void; swim?: () => void; };
 
-const move = (animal: Fish | Bird | Human) => {
-    if ("fly" in animal) {
-        animal.fly?.();
-    } else {
-        animal.swim?.();
+    const move = (animal: Fish | Bird | Human) => {
+        if ("fly" in animal) {
+            animal.fly?.();
+        } else {
+            animal.swim?.();
+        }
     }
 }
 
 //"is" operator
-/* type Dog = { run: () => void; };
-type Shark = { swim: () => void; };
+{
+    /* type Dog = { run: () => void; };
+    type Shark = { swim: () => void; };
 
-let animal: Dog | Shark = getPet();
+    let animal: Dog | Shark = getPet();
 
-const isDog = (pet: Dog | Shark): pet is Dog => "run" in pet;
+    const isDog = (pet: Dog | Shark): pet is Dog => "run" in pet;
 
-if (isDog(animal)) {
-    animal.run();
-} else {
-    animal.swim();
-} */
-
-//basic functions
-const greeter = (greetFunction: (text: string) => void) => {
-    console.log(greetFunction("Hej!"));
+    if (isDog(animal)) {
+        animal.run();
+    } else {
+        animal.swim();
+    } */
 }
 
-const printToConsole = (text: string) => text;
+//basic functions
+{
+    const greeter = (greetFunction: (text: string) => void) => {
+        console.log(greetFunction("Hej!"));
+    }
 
-greeter(printToConsole);
+    const printToConsole = (text: string) => text;
+
+    greeter(printToConsole);
+}
 
 //function with 2 types of arguments
+{
+    const map = <InputItemType, OutputItemType>(
+        array: InputItemType[],
+        transformFunction: (element: InputItemType) => OutputItemType
+    ) => array.map(transformFunction);
 
-const map = <InputItemType, OutputItemType>(
-    array: InputItemType[],
-    transformFunction: (element: InputItemType) => OutputItemType
-) => array.map(transformFunction);
+    const floatNumbers = [2.15, 4.76];
+    const transformedNumbers = map(floatNumbers, (number) => number.toFixed(1));
 
-const floatNumbers = [2.15, 4.76];
-const transformedNumbers = map(floatNumbers, (number) => number.toFixed(1));
-
-console.log(transformedNumbers);
+    console.log(transformedNumbers);
+}
 
 //function that accepts specific types of arguments
 //this function takes 2 values and return one, which is longer
+{
+    const longer = <ValueType extends { length: number }>(
+        value1: ValueType,
+        value2: ValueType) => {
+        if (value1 > value2) {
+            return value1;
+        } else {
+            return value2;
+        }
+    };
 
-const longer = <ValueType extends { length: number }>(
-    value1: ValueType,
-    value2: ValueType) => {
-    if (value1 > value2) {
-        return value1;
-    } else {
-        return value2;
-    }
-};
-
-console.log(longer("xzxz", "xxx"));
-console.log(longer([], [1]));
+    console.log(longer("xzxz", "xxx"));
+    console.log(longer([], [1]));
+}
 
 //function that merges 2 tables
+{
+    const mergeArrays = <ArrayType>(array1: ArrayType[], array2: ArrayType[]) => [
+        ...array1,
+        ...array2
+    ];
 
-const mergeArrays = <ArrayType>(array1: ArrayType[], array2: ArrayType[]) => [
-    ...array1,
-    ...array2
-];
-
-console.log(mergeArrays<number | string>([1, 2], ["opaska", "małpa"]));
+    console.log(mergeArrays<number | string>([1, 2], ["opaska", "małpa"]));
+}
 
 //functions that filters passed table
 //2 types of functions for example
+{
+    const filterArray1 = <InputType>(
+        array: InputType[],
+        filteringFunction: (item: InputType) => boolean
+    ) => array.filter(filteringFunction);
 
-const filterArray1 = <InputType>(
-    array: InputType[],
-    filteringFunction: (item: InputType) => boolean
-) => array.filter(filteringFunction);
+    const filterArray2 = <
+        ItemType,
+        FilterFunction extends (item: ItemType) => boolean
+    >(
+        array: ItemType[],
+        filterFunction: FilterFunction
+    ) => array.filter(filterFunction);
 
-const filterArray2 = <
-    ItemType,
-    FilterFunction extends (item: ItemType) => boolean
->(
-    array: ItemType[],
-    filterFunction: FilterFunction
-) => array.filter(filterFunction);
+    const numbers2 = [1, 2, 3, 4];
+    const filteredNumbers1 = filterArray1(numbers2, (number) => number % 2 === 0);
+    const filteredNumbers2 = filterArray2(numbers2, (number) => number % 2 === 0);
+    console.log(filteredNumbers1);
+    console.log(filteredNumbers2);
+}
 
-const numbers2 = [1, 2, 3, 4];
-const filteredNumbers1 = filterArray1(numbers2, (number) => number % 2 === 0);
-const filteredNumbers2 = filterArray2(numbers2, (number) => number % 2 === 0);
-console.log(filteredNumbers1);
-console.log(filteredNumbers2);
 
 //optional arguments
 //argument name can be a string or an undefined, that's why we need a ternary
-const greet = (name?: string) => {
-    console.log(`Hi${name !== undefined ? `, ${name}` : ""}`)
-}
+{
+    const greet = (name?: string) => {
+        console.log(`Hi${name !== undefined ? `, ${name}` : ""}`)
+    }
 
-greet("Adrian");
-greet();
+    greet("Adrian");
+    greet();
+}
 
 //function like map without using .map()
+{
+    const customMap = <InputItemType, OutputItemType>(
+        array: InputItemType[],
+        mapFunction: (item: InputItemType, index: number) => OutputItemType
+    ) => {
+        const newArray = [] as OutputItemType[];
 
-const customMap = <InputItemType, OutputItemType>(
-    array: InputItemType[],
-    mapFunction: (item: InputItemType, index: number) => OutputItemType
-) => {
-    const newArray = [] as OutputItemType[];
+        array.forEach((item, index) => {
+            newArray.push(mapFunction(item, index))
+        });
 
-    array.forEach((item, index) => {
-        newArray.push(mapFunction(item, index))
-    });
+        return newArray;
+    }
 
-    return newArray;
+    const animals2 = ["monkey", "donkey"];
+    console.log(animals2.map((animal) => `${animal}`));
+    console.log(customMap([1, 2, 3, 4, 5, 6], item => item + 1));
 }
-
-const animals2 = ["monkey", "donkey"];
-console.log(animals2.map((animal) => `${animal}`));
-console.log(customMap([1, 2, 3, 4, 5, 6], item => item + 1));
 
 //type object
 //type object is everything that IS NOT a primitive type
-
-const value: object = [];
+{
+    const value: object = [];
+}
 
 //type unknown
+{
+    const function1 = (value: any) => {
+        value.doSomething();
+    }
 
-/* const function1 = (value: any) => {
-    value.doSomething();
+    const function2 = (value: unknown) => {
+        //value.doSomething();
+    }
+
+    const parseJSON = (jsonString: string): unknown => JSON.parse(jsonString);
+    const array = parseJSON("blahBlah");
+    //array.push();
 }
-
-const function2 = (value: unknown) => {
-    //value.doSomething();
-}
-
-const parseJSON = (jsonString: string): unknown => JSON.parse(jsonString);
-const array = parseJSON("blahBlah");
-array.push(); */
-
 //if we have any, we can do whatever we want to a value
 //if we have unknown, we can't do anything to a value
 
 //type never
 //if a function return nothing, then TS acknowledges it as type never
-
-const error = () => {
-    throw new Error("Something went wrong!");
+{
+    const error = () => {
+        throw new Error("Something went wrong!");
+    }
 }
 
 //type function
 //this type defines any function, that gets any arguments and returns any value
 //IT IS NOT RECOMENDED TO USE IT!
+{
+    const doSomething2 = (someFunction: Function) => someFunction("text");
+    const doSomething3 = (someFunction: (text: string) => void) => someFunction("text");
 
-const doSomething2 = (someFunction: Function) => someFunction("text");
-const doSomething3 = (someFunction: (text: string) => void) => someFunction("text");
+    //rest parameters
 
-//rest parameters
+    const multiply = (factor: number, ...numbers: number[]) =>
+        numbers.map((number) => factor * number);
 
-const multiply = (factor: number, ...numbers: number[]) =>
-    numbers.map((number) => factor * number);
+    const numbers3 = [1, 2, 3];
+    console.log(multiply(5, ...numbers3));
 
-const numbers3 = [1, 2, 3];
-console.log(multiply(5, ...numbers3));
-
-const numbersToPow = [2, 5] as const;
-console.log(Math.pow(...numbersToPow));
-
-//destructuring of parameters
-
-interface RectangleArea {
-    sideA: number;
-    sideB: number;
+    const numbersToPow = [2, 5] as const;
+    console.log(Math.pow(...numbersToPow));
 }
 
-const rectangleArea = ({ sideA, sideB }: RectangleArea) => sideA * sideB;
+//destructuring of parameters
+{
+    interface RectangleArea {
+        sideA: number;
+        sideB: number;
+    }
 
-console.log(rectangleArea({ sideA: 5, sideB: 6 }));
+    const rectangleArea = ({ sideA, sideB }: RectangleArea) => sideA * sideB;
+
+    console.log(rectangleArea({ sideA: 5, sideB: 6 }));
+
+}
 
 //assigning functions with void
+{
+    type FunctionType = () => void;
 
-type FunctionType = () => void;
-
-const doNothing: FunctionType = () => {};
-const returnTrue: FunctionType = () => true;
+    const doNothing: FunctionType = () => { };
+    const returnTrue: FunctionType = () => true;
 
 
-const valueTrue = returnTrue();
+    const valueTrue = returnTrue();
+}
+
+//readonly object property
+{
+    interface SomeObject {
+        readonly property: string;
+        readonly person: {
+            name: string;
+            age: number;
+        }
+    }
+
+    const doSomething = (someObject: SomeObject) => {
+        console.log(someObject.property);
+        someObject.property = "new value";
+        someObject.person.name = "Adrian";
+        someObject.person = {};
+    }
+}
