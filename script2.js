6e21b14e203a32057620ee7862be11183507ed09
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-06-04 20:04:20 +0200

    arrays and objects testing

--- script2.js
+++ script2.js
@@ -1,5 +1,11 @@
 const countOccurencies = (array) => {
-  
+  let counts = {};
+  array.forEach(item => {
+    counts[item] ? counts[item] += 1 : counts[item] = 1;
+  })
+  return counts;
 };
 
 const array1 = ["a", "b", "a", "c", "b", "a"];
+
+console.log(countOccurencies(array1));
