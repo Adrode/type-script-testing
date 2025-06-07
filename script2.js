5cfe2b78212eb88b151ed28ad2342a1840b86c56
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-06-07 18:49:16 +0200

    groupBy - methods on objects and arrays

--- script2.js
+++ script2.js
@@ -1,10 +1,17 @@
-const invert = (object) => {
-  let inverted = {};
-  let value;
-  for (let key in object) {
-    console.log(object[key]);
-    value = object[key];
-    inverted = { ...inverted, [value]: key }
-  }
-  return inverted;
+const groupBy = (data, key) => {
+  let result = {};
+  data.forEach(element => {
+    if (!result[element[key]]) {
+      result = {
+        ...result,
+        [element[key]]: [element]
+      }
+    } else {
+      result = {
+        ...result,
+        [element[key]]: [...result[element[key]], element]
+      }
+    }
+  })
+  return result;
 };
